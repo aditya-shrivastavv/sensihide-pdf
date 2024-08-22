e9c02e0d137f27c227e1d80f37dc4d46d3da36c1
Aditya Shrivastav <aditya.shri9981@gmail.com>
2024-08-22 22:06:06 +0530

    modified dlp inspect template resource

--- terraform/redact-pdf/dlp.tf
+++ terraform/redact-pdf/dlp.tf
@@ -1,7 +1,7 @@
-resource "google_data_loss_prevention_inspect_template" "dlp_pdf_template" {
+resource "google_data_loss_prevention_inspect_template" "dlp_text_template" {
   parent       = "projects/${var.project_id}/locations/global"
-  description  = "Redact PDF Inspect Template"
-  display_name = "redact_pdf_dlp_template"
+  description  = "Redact Text Inspect Template"
+  display_name = "redact_text_dlp_template"
 
   inspect_config {
     info_types {
@@ -19,6 +19,8 @@ resource "google_data_loss_prevention_inspect_template" "dlp_pdf_template" {
     info_types {
       name = "LAST_NAME"
     }
+    min_likelihood = "POSSIBLE"
+    include_quote = true
   }
 
   depends_on = [module.project_services]
