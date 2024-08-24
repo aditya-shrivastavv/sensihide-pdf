835ccea8c35024e71928e33c3e3f43bf8fcd5bae
Aditya Shrivastav <aditya.shri9981@gmail.com>
2024-08-24 10:16:18 +0530

    removed buffer bucket from resources

--- terraform/redact-pdf/storage_bucket.tf
+++ terraform/redact-pdf/storage_bucket.tf
@@ -5,23 +5,6 @@ resource "google_storage_bucket" "input_bucket" {
   force_destroy               = true
 }
 
-resource "google_storage_bucket" "buffer_bucket" {
-  name                        = "buffer-bucket${local.app_suffix}"
-  location                    = var.region
-  uniform_bucket_level_access = true
-  force_destroy               = true
-
-  lifecycle_rule {
-    condition {
-      age = 1
-    }
-    action {
-      type = "Delete"
-    }
-  }
-
-}
-
 resource "google_storage_bucket" "output_bucket" {
   name                        = "output-bucket${local.app_suffix}"
   location                    = var.region
